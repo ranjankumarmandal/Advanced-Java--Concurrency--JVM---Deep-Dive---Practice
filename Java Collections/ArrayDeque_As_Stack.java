3c640146ae3c517e622ecd1e1fe2cb21f674f09a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-09-14 16:57:00 +0530

    Advanced Java: Java Collections: ArrayDeque as Stack

---
+++ Java Collections/ArrayDeque_As_Stack.java
@@ -0,0 +1,19 @@
+mport java.util.ArrayDeque;
+
+public class ArrayDequeStackExample {
+    public static void main(String[] args) {
+        ArrayDeque<String> pages = new ArrayDeque<>();
+
+        pages.push("Home");
+        pages.push("Products");
+        pages.push("Checkout");
+
+        System.out.println(pages);
+
+        System.out.println("Current Page: " + pages.peek());
+
+        pages.pop();
+
+        System.out.println("Current Page: " + pages.peek());
+    }
+}
