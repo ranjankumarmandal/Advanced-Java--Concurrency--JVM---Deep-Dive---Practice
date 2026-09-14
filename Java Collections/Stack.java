e61b3ded13eb18519406e29e671b2a43a23f3fbe
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-09-14 16:51:49 +0530

    Advanced Java: Java Collections: Stack

---
+++ Java Collections/Stack.java
@@ -0,0 +1,22 @@
+mport java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<String> browserHistory = new Stack<>();
+
+        browserHistory.push("google.com");
+        browserHistory.push("youtube.com");
+        browserHistory.push("github.com");
+        browserHistory.push("stackoverflow.com");
+
+        System.out.println(browserHistory);
+
+        System.out.println("Current Page: " + browserHistory.peek());
+
+        System.out.println("Going Back From: " + browserHistory.pop());
+
+        System.out.println("Current Page: " + browserHistory.peek());
+
+        System.out.println(browserHistory);
+    }
+}
