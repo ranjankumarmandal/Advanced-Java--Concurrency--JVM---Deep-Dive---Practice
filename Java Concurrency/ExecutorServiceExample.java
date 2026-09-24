2322ce8d072b828df5617ef0f2266d1aa6b85045
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-09-24 07:28:05 +0530

    Advanced Java: Java Concurrency: ExecutorService: Handling more tasks asynchronuously

--- Java Concurrency/ExecutorServiceExample.java
+++ Java Concurrency/ExecutorServiceExample.java
@@ -2,6 +2,29 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ExecutorServiceExample {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        for (int i = 1; i <= 5; i++) {
+            final int taskId = i;
+            executor.submit(() -> {
+                System.out.println("Task " + taskId + " is running on thread: " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                System.out.println("Task " + taskId + " completed");
+            });
+        }
+
+        executor.shutdown();
+        System.out.println("Executor shutdown initiated");
+    }
+}
+
+// Basic code for ExecutorService with Runnable
+class ExecutorServiceExample {
     public void learning() {
         ExecutorService executor = Executors.newFixedThreadPool(3);
 
